6a60f86341e9cf05942f54d95f9ff9f44269d0b4
bmisr <bmisr@DESKTOP-J3KHLGD.fios-router.home>
2018-10-02 20:15:25 -0400

    commit1

--- src/RectMain.java
+++ src/RectMain.java
@@ -2,20 +2,22 @@ import java.util.Scanner;
 
 public class RectMain {
 
+
 	public static void main(String[] args) 
 	{
 		
+		Scanner scan = new Scanner(System.in);
 		
-		Scanner in = new Scanner(System.in);
 		System.out.print("Enter side lengths of rectangle: ");
+		double side1 = scan.nextDouble();
+		double side2 = scan.nextDouble();
 		
-		double side1 = in.nextDouble();
-		in.nextLine();
-		double side2 = in.nextDouble();
-		in.nextLine();
 		
 		double perimeter = (side1*2) + (side2*2);
 		double area = (side1*side2);
+		double diagonal = Math.sqrt((side1 * side1) + (side2 * side2));
+		
+		System.out.printf("Rectangle perimeter: %.2f /t Rectangle area: %.2f /t Rectangle diagonal: %.2f ", perimeter, area, diagonal );
 
 	}
 
